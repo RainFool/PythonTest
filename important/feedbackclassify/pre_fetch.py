801cd432d71a3d0bd237caf05a7ae819bde08339
RainFool <rainfool1024@gmail.com>
2018-06-19 22:10:54 +0800

    initial-commit

---
+++ important/feedbackclassify/pre_fetch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+练习python urllib相关
+'''
+import requests
+
+
+def requst_feedback_data():
+    '''
+    获取反馈信息
+    '''
+    r = requests.get(
+        "http://ffilelogweb.huya.com/ajax/queryList?appId=200&page=1&pageSize=10000&isNotAutoFeedback=1")
+    # print "response code:%s,json:%s" % (r.status_code, r.text[0:100])
+    return 'u'+r.text
---
+++ urldemo/url_lib_demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+练习python urllib相关
+'''
+import requests
+
+
+def requst_feedback_data():
+    '''
+    获取反馈信息
+    '''
+    r = requests.get(
+        "http://ffilelogweb.huya.com/ajax/queryList?appId=200&page=1&pageSize=10000&isNotAutoFeedback=1")
+    print "response code:%s,json:%s" % (r.status_code, r.text)

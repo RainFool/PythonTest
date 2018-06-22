fa8ea70443143a8cb497907e86bef12ca895cdad
RainFool <rainfool1024@gmail.com>
2018-06-22 10:30:31 +0800

    反馈预处理数据为DataFrame

--- important/feedbackclassify/pre_fetch.py
+++ important/feedbackclassify/pre_fetch.py
@@ -13,4 +13,4 @@ def requst_feedback_data():
     r = requests.get(
         "http://ffilelogweb.huya.com/ajax/queryList?appId=200&page=1&pageSize=10000&isNotAutoFeedback=1")
     # print "response code:%s,json:%s" % (r.status_code, r.text[0:100])
-    return 'u'+r.text
+    return r.text.encode('utf-8')
